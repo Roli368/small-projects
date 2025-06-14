02041c6e8431feaa5fd46871455e6b9b9a2f00c1
Roli Rathour <rolirathour368@gmail.com>
2025-06-14 17:01:43 +0000

    to do list

---
+++ toDoList/todo.js
@@ -0,0 +1,29 @@
+
+ const todoInput = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todolist = document.getElementById("todo-list");
+const tasks = [];
+
+addTaskButton.addEventListener('click', () => {
+  const taskText = todoInput.value.trim();
+  if (taskText === "") return;
+
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+  todoInput.value = "";
+
+  const li = document.createElement("li");  
+  li.className = "task-item";
+  li.innerHTML = `<span>${newTask.text}</span>
+                    <button class="delete-btn">Delete</button>`;
+  li.querySelector(".delete-btn").addEventListener("click", () => {
+    todolist.removeChild(li);
+  });
+
+  todolist.appendChild(li);
+});
